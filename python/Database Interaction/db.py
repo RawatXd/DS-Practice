a908ff52122795d7651662717b16b38a882be499
Abhishek Rawat <rawatabhishek.123321@gmail.com>
2026-01-31 14:40:48 +0530

    Update db.py

--- python/Database Interaction/db.py
+++ python/Database Interaction/db.py
@@ -13,13 +13,32 @@ def get_db_cursor():
     else:
         print("Failed in Connection to a database")
 
-    cursor = connection.cursor()
+    cursor = connection.cursor(dictionary=True)
+    return connection,cursor
+
+def fetch_all_records():
+    connection,cursor = get_db_cursor()
     cursor.execute("SELECT * FROM expenses")
     expenses = cursor.fetchall()
     for expense in expenses:
         print(expense)
 
+    cursor.close()
+    connection.close()
+
+
+def fetch_expense_for_date(expense_date):
+    connection, cursor = get_db_cursor()
+    cursor.execute("SELECT * FROM expenses WHERE expense_date=%s", (expense_date,))
+    expenses = cursor.fetchall()
+    for expense in expenses:
+        print(expense)
+
+    cursor.close()
+    connection.close()
 
 
 
-connection.close()
+if __name__ == "__main__":
+    fetch_all_records()
+    fetch_expense_for_date("2024-08-01")
