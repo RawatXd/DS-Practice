85493990c2024b6c8b2f2059cd64b2a684437bfd
Abhishek Rawat <rawatabhishek.123321@gmail.com>
2026-04-03 13:58:37 +0530

    gradient_descent

---
+++ Machine Learning/Supervised Machine Learning - Regression/gradient_descent/gradient_descent.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def gradient_descent(x,y,lr=0.1,epochs = 1000):
+    m,b = 0.0,0.0
+
+    for epochs in range(epochs):
+        y_pred = m*x +b 
+        error = y - y_pred
+        cost  = np.mean(error**2)
+
+        dm = -2 * np.mean(error*x)
+        db = -2 *  np.mean(error)
+        
+        b -= db*lr
+        m -= dm*lr
+
+        print(f' m = {m} ,  b = {b} , Epoch {epochs} : Cost {cost}')
+
+
+if __name__ =="__main__":
+    x = np.array([1,2,3,4,5])
+    y = np.array([5,7,9,11,13])
+    gradient_descent(x,y)
